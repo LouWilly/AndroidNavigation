adb7538c0bfc5ab689980cd342d7e6a319e8ed54
LouWilly <mllf.flor@gmail.com>
2022-06-22 21:55:28 -0300

    nav

---
+++ app/src/main/java/com/example/navigation/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+class SecondFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        view.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_fragmentSecond_to_fragmentFirst)}
+
+        return view
+
+    }
+
+}
